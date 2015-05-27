7fe5ebb2ee27801803d8e433d291c8eed1bab69d
hcz <h.cz@qq.com>
2015-05-27 16:12:05 +0800

    add visitor_list.hpp

--- reflection++/visitor.hpp
+++ reflection++/visitor.hpp
@@ -2,35 +2,9 @@
 
 namespace rpp {
 
-struct {} visitor_head;
-
-struct VisitorBase {};
-
-struct VisitorTail {};
-
-VisitorTail visitor_next(...);
-
-#define VISITOR_REG(Type) \
-    template <class T, class Last> \
-    auto visitor_trace_##Type(T value, Last last) -> decltype( \
-        VisitorBase(value), \
-        visitor_trace_##Type(visitor_next(value), value) \
-    ); \
-    template <class Last> \
-    Last visitor_trace_##Type(VisitorTail, Last); \
-    Type visitor_next( \
-        decltype(visitor_trace_##Type(visitor_next(visitor_head), visitor_head)) \
-    );
-
-#define VISITOR_COLLECT(Type, Wrapper) \
-    template <class T, class... Args> \
-    auto visitor_trace_##Type(T value, Args... args) -> decltype( \
-        VisitorBase(value), \
-        visitor_trace_##Type(visitor_next(value), args..., value) \
-    ); \
-    template <class... Args> \
-    Wrapper<Args...> visitor_trace_##Type(VisitorTail, Args...); \
-    using Type = \
-        decltype(visitor_trace_##Type(visitor_next(visitor_head)));
+template <class Nothing = void>
+struct VisitorBase {
+    using ReturnValue = void;
+};
 
 }
---
+++ reflection++/visitor_list.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "visitor.hpp"
+
+namespace rpp {
+
+struct {} visitor_head;
+
+struct VisitorTail {};
+
+VisitorTail visitor_next(...);
+
+// register a visitor class
+#define VISITOR_REG(Type) \
+    template <class T, class Last> \
+    auto visitor_trace_##Type(T value, Last last) -> decltype( \
+        VisitorBase<>(value), \
+        visitor_trace_##Type(visitor_next(value), value) \
+    ); \
+    template <class Last> \
+    Last visitor_trace_##Type(VisitorTail, Last); \
+    Type visitor_next( \
+        decltype(visitor_trace_##Type(visitor_next(visitor_head), visitor_head)) \
+    );
+
+// pass all visitor classes to a template and get a type alias
+// using Type = Wrapper<Visitor1, Visitor2, ...>
+#define VISITOR_COLLECT(Type, Wrapper) \
+    template <class T, class... Args> \
+    auto visitor_trace2_##Type(T value, Args... args) -> decltype( \
+        VisitorBase<>(value), \
+        visitor_trace2_##Type(visitor_next(value), args..., value) \
+    ); \
+    template <class... Args> \
+    Wrapper<Args...> visitor_trace2_##Type(VisitorTail, Args...); \
+    using Type = \
+        decltype(visitor_trace2_##Type(visitor_next(visitor_head)));
+
+// ======== Usage example ========
+
+// struct V1: public VisitorBase<> {};
+// struct V2: public VisitorBase<> {};
+// namespace some_namespace {
+//     struct V3: public VisitorBase<> {};
+// }
+// using V3 = some_namespace::V3;
+
+// VISITOR_REG(V1)
+// VISITOR_REG(V2)
+// VISITOR_REG(V3)
+
+// VISITOR_COLLECT(VAll, std::tuple)
+
+// VAll all; // show the type of VAll
+
+}
