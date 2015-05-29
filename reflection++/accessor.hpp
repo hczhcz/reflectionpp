e4464c22e5f7d7a8da506071f3556cad622a1654
hcz <h.cz@qq.com>
2015-05-29 15:48:20 +0800

    add Accessor classes

--- reflection++/accessor.hpp
+++ reflection++/accessor.hpp
@@ -6,4 +6,45 @@ namespace rpp {
 template <class Nothing = void>
 struct AccessorBase {};
 
+template <class T>
+struct AccessorLocal: public AccessorBase<> {
+    T value;
+
+    AccessorLocal(T &&_value): value(_value) {}
+    AccessorLocal(const T &_value): value(_value) {}
+
+    T &access() {
+        return value;
+    }
+};
+
+template <class T, T &value>
+struct AccessorStatic: public AccessorBase<> {
+    T &access() {
+        return value;
+    }
+};
+
+template <class T>
+struct AccessorDynamic: public AccessorBase<> {
+    T &value;
+
+    AccessorDynamic(T &_value): value(_value) {}
+
+    T &access() {
+        return value;
+    }
+};
+
+template <class Object, class T, T &Object::member>
+struct AccessorMember: public AccessorBase<> {
+    Object &object;
+
+    AccessorMember(T &_object): object(_object) {}
+
+    T &access() {
+        return object.member;
+    }
+};
+
 }
