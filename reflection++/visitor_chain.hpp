05777fa7b2d62faacddb212d32d0f1da9a945136
hcz <h.cz@qq.com>
2015-06-10 22:34:00 +0800

    add multi namespace support to visitor_chain

--- reflection++/visitor_chain.hpp
+++ reflection++/visitor_chain.hpp
@@ -14,39 +14,57 @@ struct VisitorTail final {
     VisitorTail() = delete;
 };
 
-// an abstract function to get the next member of the visitor chain
-VisitorTail visitor_next(...);
+// enable the visitor chain in the current namespace
+#define RPP_VISITOR_CHAIN_INIT() \
+    namespace visitor_chain { \
+        static constexpr struct Here final {} here{}; \
+        \
+        /* an abstract function to get the next member of the visitor chain */ \
+        rpp::VisitorTail visitor_next(...); \
+        \
+        /* call visitor_next with VisitorTail is not allowed */ \
+        void visitor_next(Here, rpp::VisitorTail) = delete; \
+    }
 
-// call visitor_next with VisitorTail is not allowed
-void visitor_next(VisitorTail) = delete;
+RPP_VISITOR_CHAIN_INIT()
 
 // register a visitor class into the visitor chain
 #define RPP_VISITOR_REG(Type) \
-    template <class Last> \
-    Last visitor_trace_##Type(VisitorTail, Last); \
-    \
-    template <class T, class Last> \
-    auto visitor_trace_##Type(T value, Last last) -> decltype( \
-        visitor_trace_##Type(visitor_next(value), value) \
-    ); \
-    \
-    Type visitor_next( \
-        decltype(visitor_trace_##Type(visitor_next(visitor_head), visitor_head)) \
-    );
+    namespace visitor_chain { \
+        template <class Last> \
+        Last visitor_trace_##Type(Here, rpp::VisitorTail, Last); \
+        \
+        template <class T, class Last> \
+        auto visitor_trace_##Type(Here, T value, Last last) -> decltype( \
+            visitor_trace_##Type(here, visitor_next(here, value), value) \
+        ); \
+        \
+        Type visitor_next( \
+            Here, decltype(visitor_trace_##Type( \
+                here, visitor_next(here, rpp::visitor_head), rpp::visitor_head) \
+            ) \
+        ); \
+    }
 
 // pass all visitor classes in the chain to a type list and create a type alias
 // using Type = TypeList<Visitor1, Visitor2, ...>
 #define RPP_VISITOR_COLLECT(Type) \
-    template <class... Args> \
-    TypeList<Args...> visitor_trace2_##Type(VisitorTail, Args...); \
+    namespace visitor_chain { \
+        template <class... Args> \
+        rpp::TypeList<Args...> visitor_trace2_##Type(Here, rpp::VisitorTail, Args...); \
+        \
+        template <class T, class... Args> \
+        auto visitor_trace2_##Type(Here, T value, Args... args) -> decltype( \
+            visitor_trace2_##Type(here, visitor_next(here, value), args..., value) \
+        ); \
+        \
+        using Type = \
+            decltype(visitor_trace2_##Type( \
+                here, visitor_next(here, rpp::visitor_head)) \
+            ); \
+    } \
     \
-    template <class T, class... Args> \
-    auto visitor_trace2_##Type(T value, Args... args) -> decltype( \
-        visitor_trace2_##Type(visitor_next(value), args..., value) \
-    ); \
-    \
-    using Type = \
-        decltype(visitor_trace2_##Type(visitor_next(visitor_head)));
+    using Type = visitor_chain::Type;
 
 }
 
@@ -74,7 +92,27 @@ void visitor_next(VisitorTail) = delete;
         RPP_VISITOR_COLLECT(VisitorAll2)
 
         static_assert(std::is_same<VisitorAll1, TypeList<>>(), "");
-        static_assert(std::is_same<VisitorAll2, TypeList<Visitor1, Visitor2, Visitor3>>(), "");
+        static_assert(std::is_same<VisitorAll2, TypeList<
+            Visitor1, Visitor2, some_namespace::Visitor3
+        >>(), "");
+
+    }
+
+    namespace rpp_another_namespace {
+
+        RPP_VISITOR_CHAIN_INIT()
+
+        using Visitor1 = rpp::Visitor1;
+        struct Visitor2: public rpp::VisitorIgnoreBase<> {};
+
+        RPP_VISITOR_REG(Visitor1)
+        RPP_VISITOR_REG(Visitor2)
+
+        RPP_VISITOR_COLLECT(VisitorAll1)
+
+        static_assert(std::is_same<VisitorAll1, rpp::TypeList<
+            rpp::Visitor1, Visitor2
+        >>(), "");
 
     }
 
