8649757b3edfa46abd94b4762d4654adfb89cb4c
hcz <h.cz@qq.com>
2015-06-16 12:35:49 +0800

    add macros

--- reflection++/accessor_infer.hpp
+++ reflection++/accessor_infer.hpp
@@ -44,6 +44,14 @@ RPP_ACCESSOR_INFER_INIT()
         accessor_infer((*static_cast<Value *>(nullptr))()) \
     )::Target<Name, Value>
 
+// wrapper of RPP_HOLDER_TYPE
+#define RPP_ACCESSOR_APPEND_TYPE(Type) \
+    ::Append<RPP_ACCESSOR_GET(RPP_HOLDER_STR(#Type), RPP_HOLDER_TYPE(Type))>
+
+// wrapper of RPP_HOLDER_CONST
+#define RPP_ACCESSOR_APPEND_CONST(Value) \
+    ::Append<RPP_ACCESSOR_GET(RPP_HOLDER_STR(#Value), RPP_HOLDER_CONST(Value))>
+
 // wrapper of RPP_HOLDER_REF
 #define RPP_ACCESSOR_APPEND_REF(Value) \
     ::Append<RPP_ACCESSOR_GET(RPP_HOLDER_STR(#Value), RPP_HOLDER_REF(Value))>
@@ -54,22 +62,22 @@ RPP_ACCESSOR_INFER_INIT()
 
 // call RPP_ACCESSOR_BIND and always use HolderMember
 // some helper macros
-#define RPP_ACCESSOR_BIND_OBJECT_ALSO , RPP_ACCESSOR_BIND_OBJECT_MEMBER),
+#define RPP_ACCESSOR_LIST_APPEND , RPP_ACCESSOR_LIST),
 #ifndef ___
-    #define ___ RPP_ACCESSOR_BIND_OBJECT_ALSO
+    #define ___ RPP_ACCESSOR_LIST_APPEND
 #endif
-#define RPP_ACCESSOR_BIND_OBJECT_MEMBER_1(Method, Object, Member, Next) \
+#define RPP_ACCESSOR_LIST_1(Method, Object, Member, Next) \
     Method(Object, Member) Next##_2(Method, Object
-#define RPP_ACCESSOR_BIND_OBJECT_MEMBER_2(Method, Object, Member, Next) \
+#define RPP_ACCESSOR_LIST_2(Method, Object, Member, Next) \
     Method(Object, Member) Next##_1(Method, Object
-#define RPP_ACCESSOR_BIND_OBJECT_END_1(Method, Object) /* nothing */
-#define RPP_ACCESSOR_BIND_OBJECT_END_2(Method, Object) /* nothing */
+#define RPP_ACCESSOR_LIST_END_1(Method, Object) /* nothing */
+#define RPP_ACCESSOR_LIST_END_2(Method, Object) /* nothing */
 // the main macros
 #define RPP_ACCESSOR_BIND_OBJECT(Object, Members) \
     rpp::AccessorObjectWrap< \
         rpp::TypeList<> \
-        RPP_ACCESSOR_BIND_OBJECT_MEMBER_1( \
-            RPP_ACCESSOR_APPEND_MEMBER, Object, Members, RPP_ACCESSOR_BIND_OBJECT_END \
+        RPP_ACCESSOR_LIST_1( \
+            RPP_ACCESSOR_APPEND_MEMBER, Object, Members, RPP_ACCESSOR_LIST_END \
         ) /* extra ")" */ ) \
     > accessor_infer(Object);
 
