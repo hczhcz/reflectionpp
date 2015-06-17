df55c3b598581cd5f87d212e6bd161b66ff53c12
hcz <h.cz@qq.com>
2015-06-17 16:26:49 +0800

    add common visitors

--- reflection++/visitor_common.hpp
+++ reflection++/visitor_common.hpp
@@ -1,9 +1,51 @@
 #pragma once
 
+#include <utility>
+#include <typeinfo>
 #include "visitor_chain.hpp"
 
 namespace rpp {
 
-//
+struct VisitorType: public VisitorBase<const std::type_info &> {
+    template <class T>
+    ReturnType visit(T &value) {
+        return typeid(value);
+    }
+
+    template <class Accessor>
+    ReturnType into(Accessor &accessor) {
+        return accessor.doObjectVisit(*this);
+    }
+};
+
+RPP_VISITOR_REG(VisitorType)
+
+struct VisitorSize: public VisitorBase<std::size_t> {
+    template <class T>
+    ReturnType visit(T &value) {
+        return sizeof(value);
+    }
+
+    template <class Accessor>
+    ReturnType into(Accessor &accessor) {
+        return accessor.doObjectVisit(*this);
+    }
+};
+
+RPP_VISITOR_REG(VisitorSize)
+
+struct VisitorPointer: public VisitorBase<const void *> {
+    template <class T>
+    ReturnType visit(T &value) {
+        return static_cast<ReturnType>(&value);
+    }
+
+    template <class Accessor>
+    ReturnType into(Accessor &accessor) {
+        return accessor.doObjectVisit(*this);
+    }
+};
+
+RPP_VISITOR_REG(VisitorPointer)
 
 }
