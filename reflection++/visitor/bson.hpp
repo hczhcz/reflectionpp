a0c3ae388aa3bfb2cb89da30f0ab08242a523488
hcz <h.cz@qq.com>
2015-06-28 21:02:59 +0800

    working

--- reflection++/visitor/bson.hpp
+++ reflection++/visitor/bson.hpp
@@ -20,39 +20,14 @@
 
 namespace rpp {
 
-template <class Arr>
-struct VisitorBSONArrBase: public VisitorBase<void> {
-protected:
-    Arr &arr;
-
-    VisitorBSONArrBase(Arr &_arr):
-        arr{_arr} {}
-
-    template <class Member>
-    void add(const Member &member) {
-        arr.append(member);
-    }
-};
-
+// forward declaration
 template <class Doc>
-struct VisitorBSONDocBase: public VisitorBase<void> {
-protected:
-    Doc &doc;
-    std::string name;
+struct VisitorBSONRootDocBase;
 
-    VisitorBSONDocBase(Doc &_doc, const std::string &_name):
-        doc{_doc}, name{_name} {}
-
-    template <class Member>
-    void add(const Member &member) {
-        doc.append(kvp(name, member));
-    }
-};
-
-// render a BSON (MongoDB document) object
-template <class Base>
+// render a BSON (MongoDB data) object
+template <class Base = VisitorBSONRootDocBase<bsoncxx::builder::basic::document>>
 struct VisitorBSON: public Base {
-private:
+protected:
     template <class Accessor, class T>
     void visitPtr(Accessor &accessor, T &value) {
         if (value) {
@@ -62,49 +37,8 @@ private:
         }
     }
 
-    template <class Accessor, class T>
-    void visitArr(Accessor &accessor, T &value) {
-        using namespace std::placeholders;
-        using namespace bsoncxx::builder::basic;
-
-        add([&](sub_array arr) {
-            auto begin = std::begin(value);
-            auto end = std::end(value);
-            for (auto i = begin; i != end; ++i) {
-                VisitorBSON<
-                    VisitorBSONArrBase<sub_array>
-                > child{arr};
-
-                accessor.doMemberVisit(child, *i);
-            }
-        });
-    }
-
-    template <class Accessor, class T>
-    void visitMap(Accessor &accessor, T &value) {
-        using namespace std::placeholders;
-        using namespace bsoncxx::builder::basic;
-
-        add([&](sub_document doc) {
-            auto begin = std::begin(value);
-            auto end = std::end(value);
-            for (auto i = begin; i != end; ++i) {
-                VisitorBSON<
-                    VisitorBSONDocBase<sub_document>
-                > child{i->first, doc};
-
-                accessor.doMemberVisit(child, i->second);
-            }
-        });
-    }
-
 public:
     using Base::Base;
-
-    template <class T>
-    void visit(const T &value) {
-        add(value);
-    }
 
     // pointer
 
@@ -132,73 +66,243 @@ public:
 
     template <class Accessor, class T, rpp_size_t size>
     void into(Accessor &accessor, T (&value)[size]) {
-        visitArr(accessor, value);
+        this->visitArr(accessor, value);
     }
 
     template <class Accessor, class... Args>
     void into(Accessor &accessor, std::array<Args...> &value) {
-        visitArr(accessor, value);
+        this->visitArr(accessor, value);
     }
 
     template <class Accessor, class... Args>
     void into(Accessor &accessor, std::deque<Args...> &value) {
-        visitArr(accessor, value);
+        this->visitArr(accessor, value);
     }
 
     template <class Accessor, class... Args>
     void into(Accessor &accessor, std::forward_list<Args...> &value) {
-        visitArr(accessor, value);
+        this->visitArr(accessor, value);
     }
 
     template <class Accessor, class... Args>
     void into(Accessor &accessor, std::list<Args...> &value) {
-        visitArr(accessor, value);
+        this->visitArr(accessor, value);
     }
 
     template <class Accessor, class... Args>
     void into(Accessor &accessor, std::vector<Args...> &value) {
-        visitArr(accessor, value);
+        this->visitArr(accessor, value);
     }
 
     // map
 
     template <class Accessor, class... Args>
     void into(Accessor &accessor, std::map<Args...> &value) {
-        visitMap(accessor, value);
+        this->visitMap(accessor, value);
     }
 
     template <class Accessor, class... Args>
     void into(Accessor &accessor, std::unordered_map<Args...> &value) {
-        visitMap(accessor, value);
+        this->visitMap(accessor, value);
     }
 
     // handle different types of accessors
 
     template <class... Args>
     void operator()(AccessorSimple<Args...> &accessor) {
-        visit(accessor());
+        this->visitVal(accessor());
     }
 
     template <class... Args>
     void operator()(AccessorObject<Args...> &accessor) {
-        using namespace std::placeholders;
-        using namespace bsoncxx::builder::basic;
-
-        add([&](sub_document doc) {
-            for (rpp_size_t i = 0; i < accessor.size(); ++i) {
-                VisitorBSON<
-                    VisitorBSONDocBase<sub_document>
-                > child{accessor.getMemberName(i), doc};
-
-                accessor.doMemberVisit(child, i);
-            }
-        });
+        this->visitObj(accessor);
     }
 
     template <class... Args>
     void operator()(AccessorDynamic<Args...> &accessor) {
         into(accessor, accessor());
     }
+};
+
+// hold a document entry
+template <class Doc>
+struct VisitorBSONDocBase: public VisitorBase<void> {
+protected:
+    Doc &doc;
+    std::string name;
+
+    template <class Member>
+    void add(const Member &member) {
+        doc.append(bsoncxx::builder::basic::kvp(name, member));
+    }
+
+public:
+    VisitorBSONDocBase(Doc &_doc, const std::string &_name):
+        doc{_doc}, name{_name} {}
+};
+
+// hold an array
+template <class Arr>
+struct VisitorBSONArrBase: public VisitorBase<void> {
+protected:
+    Arr &arr;
+
+    template <class Member>
+    void add(const Member &member) {
+        arr.append(member);
+    }
+
+public:
+    VisitorBSONArrBase(Arr &_arr):
+        arr{_arr} {}
+};
+
+// build a BSON object as an item in a document or an array
+template <class Base>
+struct VisitorBSONItemBase: public Base {
+protected:
+    template <class T>
+    void visitVal(const T &value) {
+        this->add(value);
+    }
+
+    template <class Accessor, class T>
+    void visitArr(Accessor &accessor, T &value) {
+        using namespace std::placeholders;
+        using namespace bsoncxx::builder::basic;
+
+        this->add([&](sub_array arr) {
+            auto begin = std::begin(value);
+            auto end = std::end(value);
+            for (auto i = begin; i != end; ++i) {
+                VisitorBSON<
+                    VisitorBSONItemBase<
+                        VisitorBSONArrBase<sub_array>
+                    >
+                > child{arr};
+
+                accessor.doMemberVisit(child, *i);
+            }
+        });
+    }
+
+    template <class Accessor, class T>
+    void visitMap(Accessor &accessor, T &value) {
+        using namespace std::placeholders;
+        using namespace bsoncxx::builder::basic;
+
+        this->add([&](sub_document doc) {
+            auto begin = std::begin(value);
+            auto end = std::end(value);
+            for (auto i = begin; i != end; ++i) {
+                VisitorBSON<
+                    VisitorBSONItemBase<
+                        VisitorBSONDocBase<sub_document>
+                    >
+                > child{doc, i->first};
+
+                accessor.doMemberVisit(child, i->second);
+            }
+        });
+    }
+
+    template <class... Args>
+    void visitObj(AccessorObject<Args...> &accessor) {
+        using namespace std::placeholders;
+        using namespace bsoncxx::builder::basic;
+
+        this->add([&](sub_document doc) {
+            for (rpp_size_t i = 0; i < accessor.size(); ++i) {
+                VisitorBSON<
+                    VisitorBSONItemBase<
+                        VisitorBSONDocBase<sub_document>
+                    >
+                > child{doc, accessor.getMemberName(i)};
+
+                accessor.doMemberVisit(child, i);
+            }
+        });
+    }
+
+public:
+    using Base::Base;
+};
+
+// build a BSON object as a root (type: document)
+template <class Doc = bsoncxx::builder::basic::document>
+struct VisitorBSONRootDocBase: public VisitorBase<void>, public Doc {
+protected:
+    template <class T>
+    void visitVal(const T &value) = delete;
+
+    template <class Accessor, class T>
+    void visitArr(Accessor &accessor, T &value) = delete;
+
+    template <class Accessor, class T>
+    void visitMap(Accessor &accessor, T &value) {
+        using namespace std::placeholders;
+        using namespace bsoncxx::builder::basic;
+
+        auto begin = std::begin(value);
+        auto end = std::end(value);
+        for (auto i = begin; i != end; ++i) {
+            VisitorBSON<
+                VisitorBSONItemBase<
+                    VisitorBSONDocBase<sub_document>
+                >
+            > child{*this, i->first};
+
+            accessor.doMemberVisit(child, i->second);
+        }
+    }
+
+    template <class... Args>
+    void visitObj(AccessorObject<Args...> &accessor) {
+        using namespace std::placeholders;
+        using namespace bsoncxx::builder::basic;
+
+        for (rpp_size_t i = 0; i < accessor.size(); ++i) {
+            VisitorBSON<
+                VisitorBSONItemBase<
+                    VisitorBSONDocBase<sub_document>
+                >
+            > child{*this, accessor.getMemberName(i)};
+
+            accessor.doMemberVisit(child, i);
+        }
+    }
+};
+
+// build a BSON object as root (type: array)
+template <class Arr = bsoncxx::builder::basic::array>
+struct VisitorBSONRootArrBase: public VisitorBase<void>, public Arr {
+protected:
+    template <class T>
+    void visitVal(const T &value) = delete;
+
+    template <class Accessor, class T>
+    void visitArr(Accessor &accessor, T &value) {
+        using namespace std::placeholders;
+        using namespace bsoncxx::builder::basic;
+
+        auto begin = std::begin(value);
+        auto end = std::end(value);
+        for (auto i = begin; i != end; ++i) {
+            VisitorBSON<
+                VisitorBSONItemBase<
+                    VisitorBSONArrBase<sub_array>
+                >
+            > child{*this};
+
+            accessor.doMemberVisit(child, *i);
+        }
+    }
+
+    template <class Accessor, class T>
+    void visitMap(Accessor &accessor, T &value) = delete;
+
+    template <class... Args>
+    void visitObj(AccessorObject<Args...> &accessor) = delete;
 };
 
 }
